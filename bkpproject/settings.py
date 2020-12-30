c35efb175472691a96a077953c39c91e83954e26
varun <vdusane6@gmail.com>
2020-12-31 01:08:12 +0530

    sqlite

--- bkpproject/settings.py
+++ bkpproject/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'bkpproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jobs',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4'
-        }
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'jobs',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
+        # 'OPTIONS': {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        #     'charset': 'utf8mb4'
+        # }
 
     }
 }
